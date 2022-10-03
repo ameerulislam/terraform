8d95184954157b7052a675ffa11266e85d31529d
Ameer Ul Islam <ameerulislam@gmail.com>
2022-10-03 22:47:04 +0530

    fixed some alignments

--- Learning Terraform (LinkedIn Learning)/Ex_Files_Terraform/Exercise Files/prod.tf
+++ Learning Terraform (LinkedIn Learning)/Ex_Files_Terraform/Exercise Files/prod.tf
@@ -77,12 +77,12 @@ resource "aws_security_group" "prod_web" {
 module "web_app" {
   source = "./modules/web_app"
 
-  web_image_id         = var.web_image_id
-  web_instance_type    = var.web_instance_type
-  web_desired_capacity = var.web_desired_capacity
-  web_max_size         = var.web_max_size
-  web_min_size         = var.web_min_size
-  subnets              = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
-  security_groups      = [aws_security_group.prod_web.id]
-  web_app	       = "prod"
+  web_image_id          = var.web_image_id
+  web_instance_type     = var.web_instance_type
+  web_desired_capacity  = var.web_desired_capacity
+  web_max_size          = var.web_max_size
+  web_min_size          = var.web_min_size
+  subnets               = [aws_default_subnet.default_az1.id,aws_default_subnet.default_az2.id]
+  security_groups       = [aws_security_group.prod_web.id]
+  web_app               = "prod"
 }
